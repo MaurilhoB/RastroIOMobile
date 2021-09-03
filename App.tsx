ce887835832a915f798e2f7fcd4c0f0f77611c96
MaurilhoB <maurilhobatista450@gmail.com>
2021-09-03 13:14:35 -0300

    inital setup

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+};
+
+export default App;
